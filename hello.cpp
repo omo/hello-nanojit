e8dc65d134ae905aa1a47013ff36bb55e71e79e3
MORITA Hajime <omo@dodgson.org>
2008-08-24 22:29:58 +0900

    add licensing term, fix indentation

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,14 @@
-
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * The author disclaims copyright to this source code.  In place of
+ * a legal notice, here is a blessing:
+ *
+ *    May you do good and not evil.
+ *    May you find forgiveness for yourself and forgive others.
+ *    May you share freely, never taking more than you give.
+ * 
+ */
 #include <nanojit/nanojit.h>
 
 namespace nj = nanojit;
@@ -14,79 +24,79 @@ static void say_hello()
 
 nj::CallInfo builtins[] = {
 	{ /* _address_ */ (intptr_t)&say_hello, 
-		/* _argtypes */ 0, 
-		/* _cse      */ (uint8_t)false, 
-		/* _fold     */ (uint8_t)false, 
-		/* _name     */ "say_hello" }
+	  /* _argtypes */ 0, 
+	  /* _cse      */ (uint8_t)false, 
+          /* _fold     */ (uint8_t)false, 
+          /* _name     */ "say_hello" }
 };
 
 int main(int argc, char* argv[])
 {
-  nj::Fragmento* frago = new (&g_gc) nj::Fragmento(&g_core, 24);
-	verbose_only(frago->labels = new (&g_gc) nj::LabelMap(&g_core, NULL););
-	frago->assm()->setCallTable(builtins);
+    nj::Fragmento* frago = new (&g_gc) nj::Fragmento(&g_core, 24);
+    verbose_only(frago->labels = new (&g_gc) nj::LabelMap(&g_core, NULL););
+    frago->assm()->setCallTable(builtins);
 
-	nj::Fragment* f = frago->newLoop(0);
-	f->root = f;
-	f->calldepth = 0;
-	f->lirbuf = new (&g_gc) nj::LirBuffer(frago, builtins); // deleted in Fragment::onDestroy()
-	f->lirbuf->names = new (&g_gc) nj::LirNameMap(&g_gc, builtins, frago->labels); // deleted at Lirbuf::~LirBuffer()
+    nj::Fragment* f = frago->newLoop(0);
+    f->root = f;
+    f->calldepth = 0;
+    f->lirbuf = new (&g_gc) nj::LirBuffer(frago, builtins); // deleted in Fragment::onDestroy()
+    f->lirbuf->names = new (&g_gc) nj::LirNameMap(&g_gc, builtins, frago->labels); // deleted at Lirbuf::~LirBuffer()
 
-	nj::LirWriter* lir = 0;
-	nj::LirWriter* lir_buf_writer = lir = new (&g_gc) nj::LirBufWriter(f->lirbuf);
-	nj::LirWriter* lir_cse_filter = lir = new (&g_gc) nj::CseFilter(lir, &g_gc);
-	nj::LirWriter* lir_expr_filter = lir = new (&g_gc) nj::ExprFilter(lir);
+    nj::LirWriter* lir = 0;
+    nj::LirWriter* lir_buf_writer = lir = new (&g_gc) nj::LirBufWriter(f->lirbuf);
+    nj::LirWriter* lir_cse_filter = lir = new (&g_gc) nj::CseFilter(lir, &g_gc);
+    nj::LirWriter* lir_expr_filter = lir = new (&g_gc) nj::ExprFilter(lir);
 
-	frago->assm()->setError(nj::None);
+    frago->assm()->setError(nj::None);
 
-	// read function parameter
-	lir->ins0(nj::LIR_trace);
-	f->lirbuf->state = lir->insParam(0);
-	f->lirbuf->param1 = lir->insParam(1);
+    // read function parameter
+    lir->ins0(nj::LIR_trace);
+    f->lirbuf->state = lir->insParam(0);
+    f->lirbuf->param1 = lir->insParam(1);
 
-	nj::LIns* args[] = {};
-	lir->insCall(0 /* index on builtins array */, args);
+    nj::LIns* args[] = {};
+    lir->insCall(0 /* index on builtins array */, args);
 
-	assert(!frago->assm()->error());
+    assert(!frago->assm()->error());
 
-	nj::SideExit exit;
-	memset(&exit, 0, sizeof(exit));
-	exit.from = f;
-	exit.calldepth = 0;
-	exit.numGlobalSlots = 0;
-	exit.numStackSlots = 0;
-	exit.exitType = nj::LOOP_EXIT;
-	exit.ip_adj = 0;
-	exit.sp_adj = 0;
-	exit.rp_adj = 0;
+    nj::SideExit exit;
+    memset(&exit, 0, sizeof(exit));
+    exit.from = f;
+    exit.calldepth = 0;
+    exit.numGlobalSlots = 0;
+    exit.numStackSlots = 0;
+    exit.exitType = nj::LOOP_EXIT;
+    exit.ip_adj = 0;
+    exit.sp_adj = 0;
+    exit.rp_adj = 0;
 
-	ap::InterpState state;
-	state.sp  = 0;
-	state.eos = 0;
-	state.rp  = 0;
-	state.eor = 0;
-	state.gp  = 0;
-	state.cx  = 0;
+    ap::InterpState state;
+    state.sp  = 0;
+    state.eos = 0;
+    state.rp  = 0;
+    state.eor = 0;
+    state.gp  = 0;
+    state.cx  = 0;
 
-	f->lastIns = lir->insGuard(nj::LIR_x, lir->insImm(1), &exit);
-	compile(frago->assm(), f);
+    f->lastIns = lir->insGuard(nj::LIR_x, lir->insImm(1), &exit);
+    compile(frago->assm(), f);
 
-	union {
-		nj::NIns *code; 
-		nj::GuardRecord* (FASTCALL *func)(ap::InterpState*, nj::Fragment*); 
-	} u;
+    union {
+        nj::NIns *code; 
+        nj::GuardRecord* (FASTCALL *func)(ap::InterpState*, nj::Fragment*); 
+    } u;
 
-	u.code = f->code();
-	nj::GuardRecord* lr = u.func(&state, NULL);
-	assert(lr->exit->exitType == nj::LOOP_EXIT); // as specified above
+    u.code = f->code();
+    nj::GuardRecord* lr = u.func(&state, NULL);
+    assert(lr->exit->exitType == nj::LOOP_EXIT); // as specified above
 
-	delete lir_expr_filter;
-	delete lir_cse_filter;
-	delete lir_buf_writer;
+    delete lir_expr_filter;
+    delete lir_cse_filter;
+    delete lir_buf_writer;
 
-	verbose_only(delete frago->labels;);
-  delete frago;
+    verbose_only(delete frago->labels;);
+    delete frago;
 
-  return 0;
+    return 0;
 }
 
--- jstracer.cpp
+++ jstracer.cpp
@@ -1,3 +1,46 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ * vim: set ts=4 sw=4 et tw=99:
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla SpiderMonkey JavaScript 1.9 code, released
+ * May 28, 2008.
+ *
+ * The Initial Developer of the Original Code is
+ *   Brendan Eich <brendan@mozilla.org>
+ *
+ * Contributor(s):
+ *   Andreas Gal <gal@mozilla.com>
+ *   Mike Shaver <shaver@mozilla.org>
+ *   David Anderson <danderson@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either of the GNU General Public License Version 2 or later (the "GPL"),
+ * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+
 /*
  * copied from jstracer.cpp
  */
@@ -9,7 +52,7 @@ using namespace nanojit;
 
 class TreeInfo MMGC_SUBCLASS_DECL {
     nanojit::Fragment*      fragment;
-public:
+ public:
 #if 0
     JSScript*               script;
     unsigned                maxNativeStackSlots;
@@ -35,7 +78,7 @@ struct FrameInfo {
         uint32      word;       // for spdist/argc LIR store in record_JSOP_CALL
     };
 #else
-	int dummy;
+    int dummy;
 #endif
 };
 
@@ -59,17 +102,17 @@ nanojit::LirNameMap::formatGuard(LIns *i, char *out)
     ip = intptr_t(x->from->ip) + x->ip_adj;
     sprintf(out,
 #if 0
-        "%s: %s %s -> %s sp%+ld rp%+ld",
+            "%s: %s %s -> %s sp%+ld rp%+ld",
 #else
-        "%s: %s %s -> %s sp%+d rp%+d",
+            "%s: %s %s -> %s sp%+d rp%+d",
 #endif
-        formatRef(i),
-        lirNames[i->opcode()],
-        i->oprnd1()->isCond() ? formatRef(i->oprnd1()) : "",
-        labels->format((void *)ip),
-        x->sp_adj,
-        x->rp_adj
-        );
+            formatRef(i),
+            lirNames[i->opcode()],
+            i->oprnd1()->isCond() ? formatRef(i->oprnd1()) : "",
+            labels->format((void *)ip),
+            x->sp_adj,
+            x->rp_adj
+            );
 }
 #endif
 
